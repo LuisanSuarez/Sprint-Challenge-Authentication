7f05b406e972e6fdcd915811ec498c6b7d068a6d
Luisan <luisangelsc@gmail.com>
2018-10-19 11:04:01 -0500

    registers users + helper get function to see users

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,35 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs')
 
 const { authenticate } = require('./middlewares');
+
+const db = require('../database/dbConfig.js')
 
 module.exports = server => {
   server.post('/api/register', register);
   server.post('/api/login', login);
   server.get('/api/jokes', authenticate, getJokes);
+  server.get('/api/users', users)
 };
 
+
 function register(req, res) {
-  // implement user registration
+  const user = req.body
+
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash
+
+  db('users')
+    .insert(user)
+    .then(response => {
+      console.log(response);
+      res.status(201).send(response)
+    })
+    .catch(err => {
+      console.log(err.message);
+      res.status(400).send(err.message)
+    })
+
 }
 
 function login(req, res) {
@@ -27,4 +47,19 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+// ******************
+// ***Helpers*********
+// ******************
+
+
+function users(req, res) {
+  db('users')
+  .then(users => {
+    res.status(200).send(users)
+  })
+  .catch(err => {
+    res.status(400).send(err.message)
+  })
 }
